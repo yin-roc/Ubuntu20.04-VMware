d26cd8b5599fd438ce5be36c3f00ba07d8be5526
yin-roc <yin15295770968@163.com>
2024-06-07 21:35:04 +0800

    update

---
+++ workspace/VsCode_ws/test/build/CMakeFiles/calc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc.dir/src/main.c.o"
+  "CMakeFiles/calc.dir/src/main.c.o.d"
+  "bin/calc"
+  "bin/calc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
